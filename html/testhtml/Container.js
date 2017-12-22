6ec0a41a9fd8c5ef8a24bd74f0d737b3dd756c57
enhakar <enhakar@mail.ru>
2017-12-22 12:05:24 +0500

    Revert "Добавление продуктов в список покупок, отоброжение элементов продуктов из json"

--- html/testhtml/Container.js
+++
@@ -1,9 +0,0 @@
-function Container(id, myClass) {
-    this.id = id;
-    this.class = myClass;
-    this.htmlCode = '';
-}
-
-Container.prototype.render = function () {
-    return this.htmlCode;
-};
